555866b7eb7fa9ebfb32bab73b53b55a265c743d
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-11-01 23:22:43 +0000

    Float Datatype Commi

---
+++ DataTypeFloat.java
@@ -0,0 +1,25 @@
+package datatypefloat;
+
+
+/* Block Comment Added */
+
+public class DataTypeFloat
+{
+    public static void main ( String[] args )
+    {
+        float A = 20;
+        float B = 4;
+
+        System.out.println ( A + B );
+        System.out.println ( A - B );
+        System.out.println ( A * B );
+        System.out.println ( A / B );
+        System.out.println ( A++ );
+        System.out.println ( B-- );
+        System.out.println ( ++A + --B );
+        System.out.println ( ++A - --B );
+    }
+}
+
+
+
